4a21210bf6f0b87e9f7dc8a95b51369a9c5b6eeb
Klimek Mariusz <klimcio@gmail.com>
2023-10-27 14:05:04 +0200

    Initial commit

---
+++ PeselValidator/PeselValidator/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace PeselValidator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
